ec74f6ed24964f18b1983cc41d7bffab82a30393
15amitbhagat <15amitbhagat@gmail.com>
2021-07-01 17:44:51 +0530

    first

---
+++ routes/product.js
@@ -0,0 +1,36 @@
+const express=require('express');
+
+const router=express.Router();
+
+const{getProductById, createProduct, getProduct,photo, updateProduct, deleteProduct, getAllProducts, getAllUniqueCategories} =require("../controllers/product");
+const {isSignedIn, isAuthenticated, isAdmin} = require("../controllers/auth");
+const {getUserById} = require("../controllers/user");
+
+
+
+//param
+
+router.param("userId", getUserById);
+router.param("productId", getProductById);
+
+router.get("/product/create/:userId", isSignedIn, isAuthenticated,isAdmin, createProduct);
+
+router.get("/product/:productId", getProduct);
+router.get("/product/photo:productId", photo);
+
+router.delete("/product/:productId/:userId", isSignedIn, isAuthenticated, isAdmin, deleteProduct);
+router.put(
+  "/product/:productId/:userId",
+  isSignedIn,
+  isAuthenticated,
+  isAdmin,
+  updateProduct
+);
+
+router.get("/products", getAllProducts);
+
+router.get("/products/categories", getAllUniqueCategories);
+
+
+
+module.exports=router;
